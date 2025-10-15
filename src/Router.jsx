224024a16e00f0da4e9e0c9ad7b65c0fa91eb30c
Dokari <ricardotejedor18@gmail.com>
2025-10-15 16:55:20 -0500

    Se configura ruta protegida

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import App from './Pages/App.jsx'
 import Registro from './Pages/Registro.jsx'
 import Dashboard from './Pages/Dashboard.jsx'
+
+function RequireAuth({ children }) {
+  const usuarioStr = localStorage.getItem('usuario');
+  const isAuthed = !!usuarioStr;
+  return isAuthed ? children : <Navigate to="/iniciar-sesion" replace />;
+}
 function Router() {
   return (
     <Routes>
       <Route path="/iniciar-sesion" element={<App />} />
       <Route path="/registro" element={<Registro />} />
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<RequireAuth><Dashboard /></RequireAuth>} />
     </Routes>
   );
 }
